2c340887621c65c86d2097cc03983ede3044ed73
Mike Likes Robots <mikelikesrobots@outlook.com>
2023-12-08 00:16:18 +0000

    Reset config to defaults for Python

--- python-mqtt-hello/scripts/local_deploy.sh
+++ python-mqtt-hello/scripts/local_deploy.sh
@@ -5,4 +5,4 @@ gdk component build
 sudo /greengrass/v2/bin/greengrass-cli deployment create \
   --recipeDir $PWD/greengrass-build/recipes \
   --artifactDir $PWD/greengrass-build/artifacts \
-  --merge "com.example.PythonMqttHello=1.0.7"
+  --merge "com.example.PythonMqttHello=1.0.0"
